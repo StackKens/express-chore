61fc742a2e83c5348a8c581a00490879a95fc251
StackKens <ofwonoalex256@gmail.com>
2026-06-26 10:02:50 +0300

    chore: add req params

--- server.js
+++ server.js
@@ -8,13 +8,16 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.get("/about", (req, res) => {
-  const users = [
-    { id: 1, name: "Alex" },
-    { id: 2, name: "stackkens" },
-    { id: 3, name: "john" },
-  ];
-  res.json(users);
+const users = [
+  { id: 1, name: "Alex" },
+  { id: 2, name: "stackkens" },
+  { id: 3, name: "john" },
+];
+
+app.get("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((u) => u.id === id);
+  res.json(user);
 });
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
