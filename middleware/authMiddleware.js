f433655717f8683871843ad726249b901dc401a4
StackKens <ofwonoalex256@gmail.com>
2026-06-26 13:36:47 +0300

    feat: create authMiddleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,10 @@
+function authMiddleWare(req, res, next) {
+  const token = req.headers["authorization"];
+  if (!token) {
+    return res.status(401).json({ message: "access denied" });
+  }
+
+  next();
+}
+
+module.exports = authMiddleWare;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const authMiddleWare = require("./middleware/authMiddleware");
 const app = express();
 
 const PORT = 8000;
@@ -8,11 +9,12 @@ const PORT = 8000;
 //middleware
 
 app.use(morgan("dev"));
+app.use(express.json());
 
-// app.use((req, res, next) => {
-//   console.log(`${req.method}  ${req.url}`);
-//   // next();
-// });
+app.use((req, res, next) => {
+  console.log(`${req.method}  ${req.url}`);
+  next();
+});
 
 //get request
 app.get("/", (req, res) => {
@@ -37,10 +39,14 @@ app.get("/users/:id", (req, res) => {
 
 //post request
 
-app.use(express.json());
-
 app.post("/api/auth/user/login", (req, res) => {
   console.log(req.body);
   res.json(req.body);
+});
+
+//dashboard route
+
+app.get("/dashboard", authMiddleWare, (req, res) => {
+  res.json({ message: " Welcome to the dashboard!" });
 });
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
