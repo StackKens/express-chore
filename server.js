eb7b3ab2443cb5812df7a67378e8ec7eb7399069
StackKens <ofwonoalex256@gmail.com>
2026-06-26 09:33:11 +0300

    feat: put users dammy data in a variable

--- server.js
+++ server.js
@@ -9,11 +9,12 @@ app.get("/", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  res.json([
+  const users = [
     { id: 1, name: "Alex" },
     { id: 2, name: "stackkens" },
     { id: 3, name: "john" },
-  ]);
+  ];
+  res.json(users);
 });
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
