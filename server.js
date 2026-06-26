2c12e464e7286e446ed7948b0efd57a6c4229d20
StackKens <ofwonoalex256@gmail.com>
2026-06-26 09:27:52 +0300

    feat:add /about endpoint with raw data in json

--- server.js
+++ server.js
@@ -8,4 +8,12 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+app.get("/about", (req, res) => {
+  res.json([
+    { id: 1, name: "Alex" },
+    { id: 2, name: "stackkens" },
+    { id: 3, name: "john" },
+  ]);
+});
+
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
