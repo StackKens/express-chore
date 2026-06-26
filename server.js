57cbaf4ede467b9f30eb82cd9b5b175bff88a86a
StackKens <ofwonoalex256@gmail.com>
2026-06-26 08:40:32 +0300

    chore: add a route with get method

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ const app = express();
 
 const PORT = 8000;
 
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
